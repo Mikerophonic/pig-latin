39aee12ad1d73c45665a9ec5b83c12267f76518c
Mikerophonic <micgcon@gmail.com>
2023-06-26 11:33:18 -0700

    write forEach to make the function translate each word in a sentence

--- js/scripts.js
+++ js/scripts.js
@@ -6,43 +6,49 @@ function pigLatin(text) {
     const nonABCChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     const ending = "ay"
     const processedText = text.trim().toLowerCase();
-    let pigText = "";
+
 
     if (processedText.length === 0) {
         return "Please enter a word or sentence."
     } 
 
-    const firstChar = processedText.charAt(0);
-    const secondChar = processedText.charAt(1);
-    const thirdChar = processedText.charAt(2);
     
-    if (nonABCChars.includes(firstChar)) {
-        pigText = text;
-    } 
-    else if (vowels.includes(firstChar)) {
-        pigText = text + "w" + ending;
-    } 
-    else if (firstChar === "q" && secondChar === "u") {
-        pigText = text.slice(2) + firstChar + secondChar + ending;
-    } 
-    else if (!vowels.includes(firstChar) && secondChar === "q" && thirdChar === "u") {
-        pigText = text.slice(3) + firstChar + secondChar + thirdChar + ending;
-    }
-    else if (!vowels.includes(firstChar) && !vowels.includes(secondChar) && !vowels.includes(thirdChar)) {
-        pigText = text.slice(3) + firstChar + secondChar + thirdChar + ending;
-    }
-    else if (!vowels.includes(firstChar) && !vowels.includes(secondChar)) {
-        pigText = text.slice(2) + firstChar + secondChar + ending;
-    } else {
-        pigText = text.slice(1) + firstChar + ending;
-    }
 
-return pigText;
-
+    let textArray = processedText.split(" ");
+    
+    textArray.forEach(function(word, index) {
+        const firstChar = word.charAt(0);
+        const secondChar = word.charAt(1);
+        const thirdChar = word.charAt(2);
+        if (nonABCChars.includes(firstChar)) {
+            pigWord = word;
+        } 
+        else if (vowels.includes(firstChar)) {
+            pigWord = word + "w" + ending;
+        } 
+        else if (firstChar === "q" && secondChar === "u") {
+            pigWord = word.slice(2) + firstChar + secondChar + ending;
+        } 
+        else if (!vowels.includes(firstChar) && secondChar === "q" && thirdChar === "u") {
+            pigWord = word.slice(3) + firstChar + secondChar + thirdChar + ending;
+        }
+        else if (!vowels.includes(firstChar) && !vowels.includes(secondChar) && !vowels.includes(thirdChar)) {
+            pigWord = word.slice(3) + firstChar + secondChar + thirdChar + ending;
+        }
+        else if (!vowels.includes(firstChar) && !vowels.includes(secondChar)) {
+            pigWord = word.slice(2) + firstChar + secondChar + ending;
+        } else {
+            pigWord = word.slice(1) + firstChar + ending;
+        }
+        return textArray[index] = pigWord
+    });
+    textArray = textArray.join(" ");
+    return textArray.charAt(0).toUpperCase() + textArray.slice(1);
 }
 
 
-//UI Logic
+
+/* //UI Logic
 let text = document.querySelector("textarea").value;
 let p = document.querySelector("p");
 let button = document.querySelector("button");
@@ -50,3 +56,4 @@ let button = document.querySelector("button");
 button.addEventListener("click", function (event) {
     p.innerText = pigLatin(text);
 });
+ */
