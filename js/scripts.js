740d2b13d060a3f63c5dde4fe32f4c27cae7cd18
Mikerophonic <micgcon@gmail.com>
2023-06-25 20:30:46 -0700

    write function that translates single word into pig Latin

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function pigLatin(text) {
+    
+    const vowels = ["a", "e", "i", "o", "u"];
+    const nonABCChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    const ending = "ay"
+    let processedText = text.trim().toLowerCase();
+    let pigText = "";
+
+    if (processedText.length === 0) {
+        return "Please enter a word or sentence."
+    } 
+
+    const firstChar = processedText.charAt(0);
+    const secondChar = processedText.charAt(1);
+    
+    if (nonABCChars.includes(firstChar)) {
+        pigText = text;
+    }
+    else if (vowels.includes(firstChar)) {
+        pigText = text + "w" + ending;
+    } else if (!vowels.includes(firstChar) && !vowels.includes(secondChar)) {
+        pigText = text.slice(2) + firstChar + secondChar + ending;
+    } else {
+        pigText = text.slice(1) + firstChar + ending;
+    }
+
+return pigText;
+
+}
