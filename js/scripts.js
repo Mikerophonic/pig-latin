127406fabfd8863f246376ad8125105d737290e2
Mikerophonic <micgcon@gmail.com>
2023-06-25 20:42:28 -0700

    add conditions so that it correctly translates words tha begin with qu- and squ-

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function pigLatin(text) {
     const vowels = ["a", "e", "i", "o", "u"];
     const nonABCChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     const ending = "ay"
-    let processedText = text.trim().toLowerCase();
+    const processedText = text.trim().toLowerCase();
     let pigText = "";
 
     if (processedText.length === 0) {
@@ -12,13 +12,21 @@ function pigLatin(text) {
 
     const firstChar = processedText.charAt(0);
     const secondChar = processedText.charAt(1);
+    const thirdChar = processedText.charAt(2);
     
     if (nonABCChars.includes(firstChar)) {
         pigText = text;
-    }
+    } 
     else if (vowels.includes(firstChar)) {
         pigText = text + "w" + ending;
-    } else if (!vowels.includes(firstChar) && !vowels.includes(secondChar)) {
+    } 
+    else if (firstChar === "q" && secondChar === "u") {
+        pigText = text.slice(2) + firstChar + secondChar + ending;
+    } 
+    else if (!vowels.includes(firstChar) && secondChar === "q" && thirdChar === "u") {
+        pigText = text.slice(3) + firstChar + secondChar + thirdChar + ending;
+    }
+    else if (!vowels.includes(firstChar) && !vowels.includes(secondChar)) {
         pigText = text.slice(2) + firstChar + secondChar + ending;
     } else {
         pigText = text.slice(1) + firstChar + ending;
